12e48311c405c33baea5b3afe06539aef66fb478
Chirag Katariya <chiragkatariya@Chirags-MacBook-Air.local>
2026-06-06 11:19:56 +0530

    Initial React and Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+
+      <h1 className="text-5xl font-bold">
+        TrustHire AI
+      </h1>
+
+    </div>
+  );
+}
+
+export default App;
